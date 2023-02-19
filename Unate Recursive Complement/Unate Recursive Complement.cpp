ebad2f0860f2832f50247c7a6092586c500cea78
MostafaWahiep <20P2981@eng.asu.edu.eg>
2023-02-19 02:34:34 +0200

    implemented MostBinate()

--- Unate Recursive Complement/Unate Recursive Complement.cpp
+++ Unate Recursive Complement/Unate Recursive Complement.cpp
@@ -2,8 +2,8 @@
 //
 
 #include <iostream>
-#include <fstream>
 #include <deque>
+#include <algorithm>
 #include "CubeList.h"
 #include "Cube.h"
 
@@ -37,17 +37,46 @@ void complementOneCubed(deque<Cube>& F, int& number_of_variables) {
 bool isBinate(deque<Cube>& F, int& number_of_variables) {
 	int num = 0;
 	for (int i = 0; i < number_of_variables; i++) {
-		int x = 0, y = 0;
+		int T = 0, C = 0;
 		for (int j = 0; j < F.size(); j++) {
 			if (F[j].getValue(i + 1) == 1)
-				x++;
+				T++;
 			else if (F[j].getValue(i + 1) == 2)
-				y++;
+				C++;
 		}
-		if (x > 0 && y > 0)
+		if (T > 0 && C > 0)
 			num++;
 	}
 	return num > 0;
+}
+
+int MostBinate(deque<Cube>& F, int& number_of_variables) {
+
+	vector < pair<pair<int, int>, int>> ties;
+
+	for (int i = 0; i < number_of_variables; i++) {
+		int T = 0, C = 0, O = 0;
+		for (int j = 0; j < F.size(); j++) {
+			if (F[j].getValue(i + 1) == 1)
+				T++, O++;
+			else if (F[j].getValue(i + 1) == 2)
+				C++, O++;
+		}
+
+		// if binate
+		if (T > 0 && C > 0)
+			ties.push_back(make_pair(make_pair(O, abs(T - C)), i));
+	}
+
+	sort(ties.begin(), ties.end(), [](pair<pair<int, int>, int>& a, pair<pair<int, int>, int>& b) {
+		if (a.first.first != b.first.first)
+			return a.first.first > b.first.first;
+		if (a.first.second != b.first.second)
+			return a.first.second < b.first.second;
+		return a.second < b.second;
+		});
+
+	return ties.front().second;
 }
 
 void Complement(deque<Cube>& F, int& number_of_variables) {
@@ -66,7 +95,7 @@ void Complement(deque<Cube>& F, int& number_of_variables) {
 	}
 	else {
 
-		if (is_binate(F)) {
+		if (isBinate(F,number_of_variables)) {
 
 		}
 
